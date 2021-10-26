1dc2f1fd5776a4f1b612b53670a3a882e3d317e7
Ilyes Grati <ilyes.grati@ensi-uma.tn>
2021-10-26 20:52:50 +0100

    adding capture position tests

--- front/src/main.ts
+++ front/src/main.ts
@@ -10,3 +10,38 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+//*******CAPTURE POSITION TEST******/
+var tid = setTimeout(loopCoords, 2000)
+function loopCoords() {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    console.log(lat + "    " + long);
+  });
+  setTimeout(function () {
+    navigator.geolocation.watchPosition(function (position) {
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
+      console.log(lat + "    " + long);
+    });
+  }, 3000)
+  tid = setTimeout(loopCoords, 2000)
+  var i = 0;
+  /*while (i < 100) {
+    (function (i) {
+      setTimeout(function () {
+        navigator.geolocation.wa
+        tchPosition(function (position) {
+          let lat = position.coords.latitude;
+          let long = position.coords.longitude;
+          console.log(lat + "    " + long);
+        });
+      }, 3000)
+    })(i++)
+  }*/
+};
+function abortTimer() {
+  clearTimeout(tid)
+}
