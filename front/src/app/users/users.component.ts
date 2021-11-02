bcfa07cca2c921c2c3adf98a4b3a7f5a0f1da431
Maissa Soussi <maissa.soussi@ensi-uma.tn>
2021-11-02 11:07:07 +0100

    Users List

--- front/src/app/users/users.component.ts
+++ front/src/app/users/users.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  public users: any[] = []
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>("http://localhost:5000/user")
+        .subscribe(
+          (result) => {
+            this.users = result;
+          },
+          (error) => { }
+        )
   }
 
 }
