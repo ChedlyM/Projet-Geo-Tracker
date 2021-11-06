c352a7b2b7949c6534147ed9ea64172c3d266daf
Ilyes Grati <ilyes.grati@ensi-uma.tn>
2021-11-06 11:11:45 +0100

    adding apis

--- server/gpxapi.py
+++ server/gpxapi.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-from datetime import datetime
+from datetime import datetime, time
 
 import gpxpy
 import gpxpy.gpx
@@ -17,6 +17,222 @@ db = SQLAlchemy(app)
 CORS(app)
 
 
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    role = db.Column(db.String(255))
+
+    def __init__(self, name, password, role):
+        self.name = name
+        self.password = password
+        self.role = role
+
+    def __repr__(self):
+        return '%s/%s/%s/%s' % (self.id, self.name, self.password, self.role)
+
+
+@app.route('/user/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        body = request.json
+        name = body['username']
+        password = body['password']
+        data = User.query.order_by(User.id).all()
+        for i in range(len(data)):
+            if((name == str(data[i]).split('/')[1]) and (password == str(data[i]).split('/')[2])):
+                return jsonify({
+                    'id': str(data[i]).split('/')[0],
+                    'name': name,
+                    'password': password,
+                    'role': str(data[i]).split('/')[3]
+                })
+
+
+@app.route('/user', methods=['POST', 'GET'])
+def data():
+
+    # POST a data to database
+    if request.method == 'POST':
+        body = request.json
+        name = body['name']
+        password = body['password']
+        role = body['role']
+
+        data = User(name, password, role)
+        db.session.add(data)
+        db.session.commit()
+
+        return jsonify({
+            'name': name,
+            'password': password,
+            'role': role
+        })
+
+    # GET all data from database & sort by id
+    if request.method == 'GET':
+        # data = User.query.all()
+        data = User.query.order_by(User.id).all()
+        print(data)
+        dataJson = []
+        for i in range(len(data)):
+            if (str(data[i]).split('/')[3] == "User"):
+                dataDict = {
+                    'id': str(data[i]).split('/')[0],
+                    'name': str(data[i]).split('/')[1],
+                    'password': str(data[i]).split('/')[2],
+                    'role': str(data[i]).split('/')[3]
+                }
+                dataJson.append(dataDict)
+        return jsonify(dataJson)
+
+
+@app.route('/user/<string:id>', methods=['GET', 'DELETE', 'PUT'])
+def onedata(id):
+
+    # GET a specific data by id
+    if request.method == 'GET':
+        data = User.query.get(id)
+        print(data)
+        dataDict = {
+            'id': str(data).split('/')[0],
+            'name': str(data).split('/')[1],
+            'password': str(data).split('/')[2],
+            'role': str(data).split('/')[3],
+        }
+        return jsonify(dataDict)
+
+    # DELETE a data
+    if request.method == 'DELETE':
+        delData = User.query.filter_by(id=id).first()
+        db.session.delete(delData)
+        db.session.commit()
+        return jsonify({'status': 'Data '+id+' is deleted from PostgreSQL!'})
+
+    # UPDATE a data by id
+    if request.method == 'PUT':
+        body = request.json
+        newName = body['name']
+        newPassword = body['password']
+        newRole = body['role']
+        editData = User.query.filter_by(id=id).first()
+        editData.name = newName
+        editData.password = newPassword
+        editData.role = newRole
+        db.session.commit()
+        return jsonify({'status': 'Data '+id+' is updated from PostgreSQL!'})
+
+
+class Activity(db.Model):
+    __tablename__ = "activity"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    distance = db.Column(db.Integer)
+    date = db.Column(db.Date)
+    comments = db.Column(db.String(255))
+    gpxData = db.Column(db.String(255))
+
+    def __init__(self, name, distance, date, comments, gpxData):
+        self.name = name
+        self.distance = distance
+        self.date = date
+        self.comments = comments
+        self.gpxData = gpxData
+
+    def __repr__(self):
+        return '%s/%s/%s/%s/%s/%s' % (self.id, self.name, self.distance, self.date, self.comments, self.gpxData)
+
+
+@app.route('/activity', methods=['POST', 'GET'])
+def dataActivity():
+
+    # POST a data to database
+    if request.method == 'POST':
+        body = request.json
+        name = body['name']
+        distance = body['distance']
+        date = body['date']
+        comments = body['comments']
+        gpxData = body['gpxData']
+
+        data = Activity(name, distance, date, comments, gpxData)
+        db.session.add(data)
+        db.session.commit()
+
+        return jsonify({
+            'status': 'Data is posted to PostgreSQL!',
+            'name': name,
+            'distance': distance,
+            'date': date,
+            'comments': comments,
+            'gpxData': gpxData
+        })
+
+    # GET all data from database & sort by id
+    if request.method == 'GET':
+        # data = Activity.query.all()
+        data = Activity.query.order_by(Activity.id).all()
+        print(data)
+        dataJson = []
+        for i in range(len(data)):
+            # print(str(data[i]).split('/'))
+            dataDict = {
+                'id': str(data[i]).split('/')[0],
+                'name': str(data[i]).split('/')[1],
+                'distance': str(data[i]).split('/')[2],
+                'date': str(data[i]).split('/')[3],
+                'comments': str(data[i]).split('/')[4],
+                'gpxData': str(data[i]).split('/')[5]
+            }
+            dataJson.append(dataDict)
+        return jsonify(dataJson)
+
+
+@app.route('/activity/<string:id>', methods=['GET', 'DELETE', 'PUT'])
+def onedataActivity(id):
+
+    # GET a specific data by id
+    if request.method == 'GET':
+        data = Activity.query.get(id)
+        print(data)
+        dataDict = {
+            'id': str(data).split('/')[0],
+            'name': str(data).split('/')[1],
+            'distance': str(data).split('/')[2],
+            'date': str(data).split('/')[3],
+            'comments': str(data).split('/')[4],
+            'gpxData': str(data).split('/')[5]
+        }
+        return jsonify(dataDict)
+
+    # DELETE a data
+    if request.method == 'DELETE':
+        delData = Activity.query.filter_by(id=id).first()
+        db.session.delete(delData)
+        db.session.commit()
+        return jsonify({'status': 'Data '+id+' is deleted from PostgreSQL!'})
+
+    # UPDATE a data by id
+    if request.method == 'PUT':
+        body = request.json
+        newName = body['name']
+        newDistance = body['distance']
+        newDate = body['date']
+        newComments = body['comments']
+        newGpxData = body['gpxData']
+        editData = Activity.query.filter_by(id=id).first()
+        editData.name = newName
+        editData.distance = newDistance
+        editData.date = newDate
+        editData.comments = newComments
+        editData.gpxData = newGpxData
+        db.session.commit()
+        return jsonify({'status': 'Data '+id+' is updated from PostgreSQL!'})
+
+###############################
+
+
 class GpxData(db.Model):
     __tablename__ = "gpxdata"
     id = db.Column(db.Integer, primary_key=True)
@@ -25,18 +241,19 @@ class GpxData(db.Model):
     long = db.Column(db.Float)
     elevation = db.Column(db.Float)
     time = db.Column(db.String(255))
+    idUser = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, idtrajet, lat, long, elevation, time):
+    def __init__(self, idtrajet, lat, long, elevation, time, idUser):
         self.idtrajet = idtrajet
         self.lat = lat
         self.long = long
         self.elevation = elevation
         self.time = time
+        self.idUser = idUser
 
 
 @app.route('/gpxdata', methods=['POST', 'GET'])
 def gpxData():
-    
 
     # POST a data to database
     if request.method == 'POST':
@@ -45,12 +262,13 @@ def gpxData():
         lat = body['lat']
         long = body['long']
         elevation = body['elevation']
+        idUser = body['idUser']
 
         # save real time tracking
         now = datetime.now()
-        current_time = now.strftime("%Y-%m-%d")#T%H:%M:%SZ
+        current_time = now.strftime("%Y-%m-%d")  # T%H:%M:%SZ
 
-        data = GpxData(idtrajet, lat, long, elevation, current_time)
+        data = GpxData(idtrajet, lat, long, elevation, current_time, idUser)
         db.session.add(data)
         db.session.commit()
 
@@ -60,7 +278,8 @@ def gpxData():
             'lat': lat,
             'long': long,
             'elevation': elevation,
-            'time': current_time
+            'time': current_time,
+            'idUser': idUser
         })
 
     # GET all data from database & sort by id
@@ -75,12 +294,33 @@ def gpxData():
                 'lat': line.lat,
                 'long': line.long,
                 'elevation': line.elevation,
-                'time': line.time
+                'time': line.time,
+                'idUser': line.idUser
             }
             dataJson.append(dataDict)
         return jsonify(dataJson)
 
+# GET last user position
 
+
+@app.route('/lastuserpos/<idUser>', methods=['GET'])
+def getLastUserPos(idUser=None):
+    # GET all data from database & sort by id
+    if request.method == 'GET':
+        data = GpxData.query.filter_by(idUser=str(idUser))
+        data = data[::-1]
+        dataJson = []
+        dataDict = {}
+        dataDict = {
+            'lat': data[0].lat,
+            'long': data[0].long,
+            'elevation': data[0].elevation
+        }
+        dataJson.append(dataDict)
+        return jsonify(dataJson)
+
+
+"""
 # API for creating gpx file (POST : gpxfile/id ==> id.gpx)
 @app.route('/gpxfile/<idtrajet>', methods=['POST'])
 def createGPX(idtrajet=None):
@@ -121,7 +361,68 @@ def createGPX(idtrajet=None):
         return jsonify({
             'status': 'GPX file created successfully!'
         })
+"""
 
+
+# API for creating gpx file (POST : gpxfile/date/idUser ==> date_idUser.gpx)
+@app.route('/gpxfile/<date>/<idUser>', methods=['POST'])
+def createGPX(date=None, idUser=None):
+
+    # POST a data to database
+    if request.method == 'POST':
+
+        # Creating a new file:
+        # --------------------
+
+        gpx = gpxpy.gpx.GPX()
+
+        # Create first track in our GPX:
+        gpx_track = gpxpy.gpx.GPXTrack()
+        gpx.tracks.append(gpx_track)
+
+        # Create first segment in our GPX track:
+        gpx_segment = gpxpy.gpx.GPXTrackSegment()
+        gpx_track.segments.append(gpx_segment)
+
+        # Create route points:
+        trajetPoints = GpxData.query.filter_by(
+            time=str(date), idUser=idUser)  # id user/ date
+        for point in trajetPoints:
+            gpx_segment.points.append(
+                gpxpy.gpx.GPXTrackPoint(point.lat, point.long, point.elevation))
+
+        # You can add routes and waypoints, too...
+
+        print('Created GPX:', gpx.to_xml())
+
+        # Write gpx file
+        filename = "./assets/gpx/" + str(date) + "_" + str(idUser) + ".gpx"
+        outF = open(filename, "w")
+        outF.write(gpx.to_xml())
+        outF.close()
+
+        return jsonify({
+            'status': 'GPX file created successfully!'
+        })
+
+# API for getting all routes by user (POST : gpxdata/idUser ==> {idTrajet, time})
+
+
+@app.route('/gpxuserdata/<idUser>', methods=['GET'])
+def gpxUserData(idUser=None):
+
+    # GET all data from database & sort by id
+    if request.method == 'GET':
+        data = GpxData.query.filter_by(idUser=str(idUser))
+        dataJson = []
+        dataDict = {}
+        for line in data:  # loop through the user data
+            dataDict = {
+                'idtrajet': line.idtrajet,
+                'time': line.time
+            }
+            dataJson.append(dataDict)
+        return jsonify(dataJson)
 
 
 if __name__ == '__main__':
