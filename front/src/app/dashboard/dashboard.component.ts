600bec3f8746bd6cb146b4f9610a2e340e62ab4d
Maissa Soussi <maissa.soussi@ensi-uma.tn>
2021-11-08 19:41:42 +0100

    maps for last position

--- front/src/app/dashboard/dashboard.component.ts
+++ front/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import * as L from 'leaflet';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -9,10 +10,24 @@ import { HttpClient } from '@angular/common/http';
 export class DashboardComponent implements OnInit {
   role : any;
   id : any
-  days : any
+  days : any 
   title = 'GEOTRACKER';
+  private centroid: L.LatLngExpression = [35.96099, 9.68194];
   constructor(private router: Router, private _Activatedroute: ActivatedRoute, private http: HttpClient) { }
+  private initMap(): void {
+    var map = L.map('map', {
+      center: this.centroid,
+      zoom: 4
+    });
+    const tiles = L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=57XMBquwc7haRCgQDYWX', {
+      maxZoom: 24,
+      minZoom: 7,
+      attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+    });
 
+    tiles.addTo(map);
+    var marker = L.marker([36.836893, 10.235563]).addTo(map);
+  }
   generer_gpx(date:any){
     this.http.post("http://127.0.0.1:5000/gpxfile/" + date + "/" + this.id, null).subscribe(
       (data)=>{
@@ -28,6 +43,8 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.initMap();
+    
     this.id = this._Activatedroute.snapshot.paramMap.get("id");
     this.role = localStorage.getItem("role")
     if (this.role != null)
